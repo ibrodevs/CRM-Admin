02f37f76064e850af6d7ddb3d6e8a35bc03cebb7
Ibrokhim <132742784+ibrodevs@users.noreply.github.com>
2026-07-21 16:39:25 +0600

    feat: activate backend bridge for legacy screens

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,13 @@
 
 import dynamic from 'next/dynamic';
 
-
-
 const CRMApp = dynamic(() => import('../js/app'), { ssr: false });
+const LegacyBackendBridge = dynamic(() => import('../js/core/legacy-backend-bridge'), { ssr: false });
 
 export default function Page() {
-
   return (
     <div id="root">
+      <LegacyBackendBridge />
       <CRMApp />
     </div>
   );
