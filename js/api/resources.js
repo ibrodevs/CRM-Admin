b9c8a45fe3704ef383196a638cab0828ca08c750
ibrodevs <ibroabdraimov20@gmail.com>
2026-09-06 18:47:51 +0600

    refactor: isolate financeApi domain API

--- js/api/resources.js
+++ js/api/resources.js
@@ -46,25 +46,7 @@ export { bookingApi } from '../../src/modules/orders/api.js';
 
 export { groupsApi } from '../../src/modules/orders/api.js';
 
-export const financeApi = {
-  overview: workspaceApi.financeOverview,
-  companySummary: (companyId, signal) => get(`companies/${companyId}/finance-summary/`, signal),
-  accounts: (signal) => get('finance/accounts/', signal),
-  transactions: workspaceApi.transactions,
-  obligations: (params = {}, signal) => list('finance/obligations/', { page_size: 100, ...params }, signal),
-  createObligation: (body) => create('finance/obligations/', body),
-  payments: (params = {}, signal) => list('finance/payments/', { page_size: 100, ...params }, signal),
-  createPayment: (body) => create('finance/payments/', body),
-  createDocument: (body) => create('finance/documents/', body),
-  confirmPayment: (id, body) => create(`finance/payments/${id}/confirm/`, body),
-  paymentOrderUrl: (id) => apiPath(`finance/payments/${id}/payment-order/`),
-  allocatePayment: (id, allocations) => create(`finance/payments/${id}/allocate/`, { allocations }),
-  refunds: (params = {}, signal) => list('finance/refunds/', { page_size: 100, ...params }, signal),
-  createRefund: (body) => create('finance/refunds/', body),
-  executeRefund: (id) => create(`finance/refunds/${id}/execute/`, {}),
-  cashflow: (params = {}, signal) => list('finance/cashflow/', params, signal),
-  economics: (params = {}, signal) => list('finance/economics/', params, signal),
-};
+export { financeApi } from '../../src/modules/finance/api.js';
 
 export const calendarApi = {
   feed: workspaceApi.calendar,
---
+++ src/modules/finance/api/financeApi.js
@@ -0,0 +1,23 @@
+import { apiPath, apiRequest, queryString } from '../../../shared/api/client.js';
+import { list, get, create, patch, remove } from '../../../shared/api/operations.js';
+import { workspaceApi } from '../../workspace/api.js';
+
+export const financeApi = {
+  overview: workspaceApi.financeOverview,
+  companySummary: (companyId, signal) => get(`companies/${companyId}/finance-summary/`, signal),
+  accounts: (signal) => get('finance/accounts/', signal),
+  transactions: workspaceApi.transactions,
+  obligations: (params = {}, signal) => list('finance/obligations/', { page_size: 100, ...params }, signal),
+  createObligation: (body) => create('finance/obligations/', body),
+  payments: (params = {}, signal) => list('finance/payments/', { page_size: 100, ...params }, signal),
+  createPayment: (body) => create('finance/payments/', body),
+  createDocument: (body) => create('finance/documents/', body),
+  confirmPayment: (id, body) => create(`finance/payments/${id}/confirm/`, body),
+  paymentOrderUrl: (id) => apiPath(`finance/payments/${id}/payment-order/`),
+  allocatePayment: (id, allocations) => create(`finance/payments/${id}/allocate/`, { allocations }),
+  refunds: (params = {}, signal) => list('finance/refunds/', { page_size: 100, ...params }, signal),
+  createRefund: (body) => create('finance/refunds/', body),
+  executeRefund: (id) => create(`finance/refunds/${id}/execute/`, {}),
+  cashflow: (params = {}, signal) => list('finance/cashflow/', params, signal),
+  economics: (params = {}, signal) => list('finance/economics/', params, signal),
+};
