64759f9e656c7100e5b99bbc39f321e921bcd588
ibrodevs <ibroabdraimov20@gmail.com>
2026-09-06 18:42:49 +0600

    refactor: isolate usersApi domain API

--- js/api/resources.js
+++ js/api/resources.js
@@ -14,21 +14,7 @@ export { travelPolicyApi } from '../../src/modules/companies/api.js';
 
 export { accountApi } from '../../src/modules/account/api.js';
 
-export const usersApi = {
-  list: (params = {}, signal) => list('users/', { page_size: 100, ...params }, signal),
-  create: (body) => create('users/', body),
-  update: (id, body) => patch(`users/${id}/`, body),
-  invite: (id) => create(`users/${id}/invite/`, {}),
-  suspend: (id, reason = '') => create(`users/${id}/suspend/`, { reason }),
-  roles: (signal) => get('roles/', signal),
-  updateRole: (id, permissions) => apiRequest(apiPath(`roles/${id}/`), { method: 'PUT', body: { permissions } }),
-  userRoles: (id, signal) => get(`users/${id}/roles/`, signal),
-  setRoles: (id, roles) => apiRequest(apiPath(`users/${id}/roles/`), { method: 'PUT', body: { roles } }),
-  serviceAccess: (id, signal) => get(`users/${id}/service-access/`, signal),
-  setServiceAccess: (id, body) => apiRequest(apiPath(`users/${id}/service-access/`), { method: 'PUT', body }),
-  sla: (id, signal) => get(`users/${id}/sla/`, signal),
-  setSla: (id, minutes) => apiRequest(apiPath(`users/${id}/sla/`), { method: 'PUT', body: { sla_response_minutes: minutes } }),
-};
+export { usersApi } from '../../src/modules/users/api.js';
 
 export const ordersApi = {
   list: (params = {}, signal) => list('orders/', { page_size: 100, ...params }, signal),
---
+++ src/modules/users/api/usersApi.js
@@ -0,0 +1,18 @@
+import { apiPath, apiRequest, queryString } from '../../../shared/api/client.js';
+import { list, get, create, patch, remove } from '../../../shared/api/operations.js';
+
+export const usersApi = {
+  list: (params = {}, signal) => list('users/', { page_size: 100, ...params }, signal),
+  create: (body) => create('users/', body),
+  update: (id, body) => patch(`users/${id}/`, body),
+  invite: (id) => create(`users/${id}/invite/`, {}),
+  suspend: (id, reason = '') => create(`users/${id}/suspend/`, { reason }),
+  roles: (signal) => get('roles/', signal),
+  updateRole: (id, permissions) => apiRequest(apiPath(`roles/${id}/`), { method: 'PUT', body: { permissions } }),
+  userRoles: (id, signal) => get(`users/${id}/roles/`, signal),
+  setRoles: (id, roles) => apiRequest(apiPath(`users/${id}/roles/`), { method: 'PUT', body: { roles } }),
+  serviceAccess: (id, signal) => get(`users/${id}/service-access/`, signal),
+  setServiceAccess: (id, body) => apiRequest(apiPath(`users/${id}/service-access/`), { method: 'PUT', body }),
+  sla: (id, signal) => get(`users/${id}/sla/`, signal),
+  setSla: (id, minutes) => apiRequest(apiPath(`users/${id}/sla/`), { method: 'PUT', body: { sla_response_minutes: minutes } }),
+};
